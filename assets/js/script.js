346f31c2074cdd0cc9e531a4bda3cfdf6c6ca0ce
Murillo Aguiar <aguiar_murillo@outlook.com>
2021-10-30 02:42:17 -0300

    Gravando os dados
    Gravando os dados por meio da classe data. Inserindo ano automaticamente

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,105 @@
-const body = document.querySelector("body")
-
-/*teste
-body.onload = () =>{
-    console.log('carregado')
-    fetch('https://jsonplaceholder.typicode.com/comments')
-                .then(resposta => resposta.text())
-                .then(dados => console.log('comentarios: ',dados))
-}*/
-
 class Expense{
-    constructor(day, month, category, description, value){
+    constructor(day, month, year, category, description, value){
         this.day = day
         this.month = month
+        this.year = year
         this.category = category
         this.description = description
         this.value = value
     }
 
     validate(){
-       
+        let mesage = 'Existem erros, corrijá-os antes de enviar novamente'
+
         for(let i in this){
 
 			if(this[i] == undefined || this[i] == null || this[i] == ''){
                 inputs.forEach(element=>{
                     if (element.value == "" || element.value == undefined || element.value == null) {
-                        focusOutline(element, 0)
-                        
+                        focusOutline(element, 2)
                     }
-               
                 })
-                createDivAlert("Existem campos para serem preenchidos")
+                createDivAlert("Existem campos para serem preenchidos. Complete-os e envie novamente",'erro')
                 
 				return false
 
-			}else if (i == 'day' && (this[i] <= 0 || this[i] >31)) {
+			}else if (i == 'day' && (!(Number(this[i])) || this[i] <= 0 || this[i] >31)) {
                 
-                createDivAlert("Existem erros corrija-os")
+                createDivAlert(mesage,'erro')
                 return false
                         
             }else if (i == 'description' && this[i].length <=3) {
                 
-                createDivAlert("Existem erros corrija-os")
+                createDivAlert(mesage,'erro')
                 return false
                         
             }else if (i == 'value' && (!(Number(this[i])) || this[i] <=0)) {
                 
-                createDivAlert("Existem erros corrija-os")
+                createDivAlert(mesage,'erro')
                 return false
                         
             }
-               
-            
-            
-                
-            
+
 		}
 
-        return true
-    
-        
+        return true   
     }
-}
+}// ------- /Expense
+
+class Data{
+    constructor(){
+        let id = localStorage.getItem('id')
+
+        if (id == null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+
+    nextId(){
+        let next_id = localStorage.getItem('id') 
+        next_id = parseInt(next_id)
+        next_id+=1
+        return next_id
+    }
+
+    insert(expense){
+        let id = this.nextId()
+        localStorage.setItem(id, JSON.stringify(expense))
+        localStorage.setItem('id', id)
+    }
+
+    showAllExpenses(){
+        let id = localStorage.getItem('id')
+        let expense = []
+        
+
+        for (let i = 1; i <= id; i++) {
+            let item = JSON.parse(localStorage.getItem(i))
+            console.log(item)
+
+            if (item == null) {
+                continue
+            }
+
+            item.id = i
+
+            expense.push(item)
+            
+        }
+
+        return expense
+    }
+
+
+}// ------ /Data
+
 
 
 const inputs = document.querySelectorAll('.focus')
 let button = document.querySelector("#insert")
+let data = new Data
 
-
-button.addEventListener('click', insert)
+button.addEventListener('click', submit)
 
 function deleteDivAlert(id){
 
@@ -76,16 +108,12 @@ function deleteDivAlert(id){
     if (div_alert != null) {
 
         div_alert.parentNode.removeChild(div_alert)
-
     }
-    
- 
 }
 
 function createDivAlert(message, id){
 
     deleteDivAlert(id)
-
 
     let div = document.createElement('div')
     div.className = `alert alert-danger mt-2 div${id}`
@@ -95,63 +123,81 @@ function createDivAlert(message, id){
     let section = document.querySelector(".content")
     
     section.appendChild(div)
-
-    
 }
 
-function insert(){
+function submit(){
+
+    let day = document.querySelector("#day")
+    let month = document.querySelector("#month")
+    let category = document.querySelector("#category")
+    let description = document.querySelector("#description")
+    let value = document.querySelector("#value")
+    let date = new Date()
 
     let expense = new Expense(
-        document.querySelector("#day").value,
-        document.querySelector("#month").value,
-        document.querySelector("#category").value,
-        document.querySelector("#description").value,
-        document.querySelector("#value").value,
+        day.value, 
+        month.value,
+        date.getFullYear(), 
+        category.value, 
+        description.value, 
+        Number(value.value)
     )
 
-    console.log(expense)
+    console.log()
 
     if (expense.validate()) {
+
+        inputs.forEach(element=>{
+            focusOutline(element, 3)
+            element.value = ""
+        })
+
+        deleteDivAlert('erro')
+
+        //data.insert(expense)
+        
         alert('produtos cadastrados')
     }
 }
 
 function focusOutline(element, id){
-    if (id) {
+    if (id==1) {
         element.style.border = '2px solid #22b070'
-    }else{
+    }else if(id==2){
         element.style.border = '2px solid #df6464'
+    }else if(id==3){
+        element.style.border = ''
     }
 }
 
-
 inputs.forEach(element =>{
     element.addEventListener('focusout', ()=>{
+
         if (element.value != "") {
     
-            if (element.id == 'day' && (element.value <= 0 || element.value >31)){
-                focusOutline(element, 0)
+            if (element.id == 'day' && (!(Number(element.value)) || element.value <= 0 || element.value >31)){
+                focusOutline(element, 2)
                 createDivAlert("Digite um dia entre 1 e 31", element.id)
 
             }else if(element.id == 'value' && (!(Number(element.value)) || element.value <=0)) {
-                focusOutline(element, 0)
+                focusOutline(element, 2)
                 createDivAlert("Digite um valor válido", element.id)
 
             }else if(element.id == 'description' && element.value.length <=3){
-                focusOutline(element, 0)
+                focusOutline(element, 2)
                 createDivAlert("Escreva uma descrição com mais de 3 caracteres", element.id)
-                
-
+            
             }else{
                 focusOutline(element, 1)
-                deleteDivAlert(element.id)
-                
+                deleteDivAlert(element.id)     
             }
 
         }else{
-            element.style.border = ''
+            focusOutline(element, 3)
             deleteDivAlert(element.id)
+            
         }
-    })
+    }) 
 })
+
 
